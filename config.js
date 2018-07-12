9ed7ed3e9f280524311b14d5188d1216c91cb2e0
honopg <honopg@gmail.com>
2018-07-12 11:04:36 +0200

    cambios

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	port: process.env.OPENSHIFT_NODEJS_PORT || 3001,
-	ip: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1,
+	ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
 	db: process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost:27017/app-db',
 	SECRET_TOKEN: 'miclavedetokenparamiaplicacion'
 }
